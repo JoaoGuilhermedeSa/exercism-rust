7e432a7e96aa9f5cfb7c1bb6c76d5fb3d3a5bb9c
João Guilherme de Sá <jaum_ipm@hotmail.com>
2025-10-27 23:31:53 -0300

    sum-of-multiples

---
+++ sum-of-multiples/src/lib.rs
@@ -0,0 +1,14 @@
+pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
+    let mut multiples = std::collections::HashSet::new();
+
+    for &factor in factors {
+        if factor == 0 {
+            continue;
+        }
+        for multiple in (factor..limit).step_by(factor as usize) {
+            multiples.insert(multiple);
+        }
+    }
+
+    multiples.iter().sum()
+}
